dd48d9e492334733d6ba33c85b7d91d9ec5fd79d
Luis Manuel <luis.manuel.saenz@hotmail.com>
2018-04-12 12:36:29 -0600

    correcciones

--- ProyectoResidencias/CambiodeBienes/PorEmpleados.Designer.cs
+++ ProyectoResidencias/CambiodeBienes/PorEmpleados.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Cancelar = new System.Windows.Forms.Button();
+            this.Aceptar = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.CBNombreNuevo = new System.Windows.Forms.ComboBox();
             this.GridNuevo = new System.Windows.Forms.DataGridView();
@@ -42,35 +42,41 @@
             ((System.ComponentModel.ISupportInitialize)(this.GridActual)).BeginInit();
             this.SuspendLayout();
             // 
-            // button2
+            // Cancelar
             // 
-            this.button2.BackColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(335, 293);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(124, 29);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Cerrar";
-            this.button2.UseVisualStyleBackColor = false;
+            this.Cancelar.BackColor = System.Drawing.Color.White;
+            this.Cancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancelar.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cancelar.Location = new System.Drawing.Point(433, 387);
+            this.Cancelar.Name = "Cancelar";
+            this.Cancelar.Size = new System.Drawing.Size(131, 34);
+            this.Cancelar.TabIndex = 7;
+            this.Cancelar.Text = "Cancelar";
+            this.Cancelar.UseVisualStyleBackColor = false;
+            this.Cancelar.Click += new System.EventHandler(this.Cancelar_Click);
             // 
-            // button1
+            // Aceptar
             // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(188, 293);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(124, 29);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Aceptar Cambio";
-            this.button1.UseVisualStyleBackColor = false;
+            this.Aceptar.BackColor = System.Drawing.Color.White;
+            this.Aceptar.Enabled = false;
+            this.Aceptar.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Aceptar.Location = new System.Drawing.Point(279, 387);
+            this.Aceptar.Name = "Aceptar";
+            this.Aceptar.Size = new System.Drawing.Size(133, 34);
+            this.Aceptar.TabIndex = 6;
+            this.Aceptar.Text = "Aceptar Cambio";
+            this.Aceptar.UseVisualStyleBackColor = false;
+            this.Aceptar.Click += new System.EventHandler(this.Aceptar_Click);
             // 
             // groupBox2
             // 
             this.groupBox2.BackColor = System.Drawing.Color.WhiteSmoke;
             this.groupBox2.Controls.Add(this.CBNombreNuevo);
             this.groupBox2.Controls.Add(this.GridNuevo);
-            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(329, 12);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(433, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(300, 258);
+            this.groupBox2.Size = new System.Drawing.Size(400, 350);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Resguardo del Empleado Nuevo";
@@ -83,15 +89,20 @@
             this.CBNombreNuevo.FormattingEnabled = true;
             this.CBNombreNuevo.Location = new System.Drawing.Point(6, 19);
             this.CBNombreNuevo.Name = "CBNombreNuevo";
-            this.CBNombreNuevo.Size = new System.Drawing.Size(288, 25);
+            this.CBNombreNuevo.Size = new System.Drawing.Size(388, 25);
             this.CBNombreNuevo.TabIndex = 2;
+            this.CBNombreNuevo.SelectedIndexChanged += new System.EventHandler(this.CBNombreNuevo_SelectedIndexChanged);
             // 
             // GridNuevo
             // 
+            this.GridNuevo.AllowUserToAddRows = false;
+            this.GridNuevo.AllowUserToDeleteRows = false;
+            this.GridNuevo.AllowUserToOrderColumns = true;
             this.GridNuevo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.GridNuevo.Location = new System.Drawing.Point(6, 46);
+            this.GridNuevo.Location = new System.Drawing.Point(6, 50);
             this.GridNuevo.Name = "GridNuevo";
-            this.GridNuevo.Size = new System.Drawing.Size(288, 203);
+            this.GridNuevo.ReadOnly = true;
+            this.GridNuevo.Size = new System.Drawing.Size(388, 294);
             this.GridNuevo.TabIndex = 2;
             // 
             // groupBox1
@@ -99,10 +110,10 @@
             this.groupBox1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.groupBox1.Controls.Add(this.CBNombreActual);
             this.groupBox1.Controls.Add(this.GridActual);
-            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(300, 258);
+            this.groupBox1.Size = new System.Drawing.Size(400, 350);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Resguardo del Empleado Actual";
@@ -115,16 +126,22 @@
             this.CBNombreActual.FormattingEnabled = true;
             this.CBNombreActual.Location = new System.Drawing.Point(6, 19);
             this.CBNombreActual.Name = "CBNombreActual";
-            this.CBNombreActual.Size = new System.Drawing.Size(288, 25);
+            this.CBNombreActual.Size = new System.Drawing.Size(388, 25);
             this.CBNombreActual.TabIndex = 1;
             this.CBNombreActual.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // GridActual
             // 
+            this.GridActual.AllowUserToAddRows = false;
+            this.GridActual.AllowUserToDeleteRows = false;
+            this.GridActual.AllowUserToOrderColumns = true;
+            this.GridActual.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.GridActual.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
             this.GridActual.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.GridActual.Location = new System.Drawing.Point(6, 46);
+            this.GridActual.Location = new System.Drawing.Point(6, 50);
             this.GridActual.Name = "GridActual";
-            this.GridActual.Size = new System.Drawing.Size(288, 203);
+            this.GridActual.ReadOnly = true;
+            this.GridActual.Size = new System.Drawing.Size(388, 294);
             this.GridActual.TabIndex = 0;
             // 
             // PorEmpleados
@@ -132,9 +149,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DodgerBlue;
-            this.ClientSize = new System.Drawing.Size(641, 334);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.CancelButton = this.Cancelar;
+            this.ClientSize = new System.Drawing.Size(845, 433);
+            this.Controls.Add(this.Cancelar);
+            this.Controls.Add(this.Aceptar);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "PorEmpleados";
@@ -151,8 +169,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Cancelar;
+        private System.Windows.Forms.Button Aceptar;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.ComboBox CBNombreNuevo;
         private System.Windows.Forms.DataGridView GridNuevo;
--- ProyectoResidencias/CambiodeBienes/PorEmpleados.cs
+++ ProyectoResidencias/CambiodeBienes/PorEmpleados.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ProyectoResidencias.CBienes.CambiodeBienes
 {
@@ -24,7 +25,60 @@ namespace ProyectoResidencias.CBienes.CambiodeBienes
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            MessageBox.Show("f");
+            Clases.LLenadoGrids.llenarGrid(GridActual, "Select bienes.Etiqueta, CatArticulos.Descripcion, Bienes.total as Precio," +
+                "bienes.Observacion from bienes inner join CatArticulos ON bienes.IdArticulo=CatArticulos.Id" +
+                " where bienes.NoEmpleado=(Select NoEmp from empleados where Nombre='" + CBNombreActual.SelectedItem.ToString() + "')", "bienes");
+            if (CBNombreActual.SelectedIndex >= 0 && CBNombreNuevo.SelectedIndex >= 0)
+                Aceptar.Enabled = true;
+        }
+
+        private void CBNombreNuevo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Clases.LLenadoGrids.llenarGrid(GridNuevo, "Select bienes.Etiqueta, CatArticulos.Descripcion, Bienes.total as Precio," +
+                "bienes.Observacion from bienes inner join CatArticulos ON bienes.IdArticulo=CatArticulos.Id" +
+                " where bienes.NoEmpleado=(Select NoEmp from empleados where Nombre='" + CBNombreNuevo.SelectedItem.ToString() + "')", "bienes");
+            if (CBNombreActual.SelectedIndex >= 0 && CBNombreNuevo.SelectedIndex >= 0)
+                Aceptar.Enabled = true;
+        }
+
+        private void Cancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Aceptar_Click(object sender, EventArgs e)
+        {
+            string ConnString = Clases.stconexion.scon;
+            string SqlString = "Update bienes set NoEmpleado=(select NoEmp from empleados where Nombre='"+
+                CBNombreNuevo.SelectedItem.ToString()+"') where NoEmpleado=(select NoEmp from empleados where Nombre='"+
+                CBNombreActual.SelectedItem.ToString()+"');";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(SqlString, conn))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                    }
+                }
+                MessageBox.Show("Cambio realizado correctamente.");
+                Clases.LLenadoGrids.llenarGrid(GridActual, "Select bienes.Etiqueta, CatArticulos.Descripcion, Bienes.total as Precio," +
+                "bienes.Observacion from bienes inner join CatArticulos ON bienes.IdArticulo=CatArticulos.Id" +
+                " where bienes.NoEmpleado=(Select NoEmp from empleados where Nombre='" + CBNombreActual.SelectedItem.ToString() + "')", "bienes");
+                
+                Clases.LLenadoGrids.llenarGrid(GridNuevo, "Select bienes.Etiqueta, CatArticulos.Descripcion, Bienes.total as Precio," +
+                    "bienes.Observacion from bienes inner join CatArticulos ON bienes.IdArticulo=CatArticulos.Id" +
+                    " where bienes.NoEmpleado=(Select NoEmp from empleados where Nombre='" + CBNombreNuevo.SelectedItem.ToString() + "')", "bienes");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("El cambio no pudo llevarse a cabo. \n" + ex.ToString());
+                DateTime hoy = DateTime.Today;
+            }
         }
     }
 }
