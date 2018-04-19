419c008e45830c6ddc91bea46fa53d40729baf69
jose eduardo carmona vasquez <LALO_1095@live.com.mx>
2018-04-19 09:17:30 -0600

    imprimir bines sf y acomodado de reportes

--- ProyectoResidencias/Activo/BienesSinFactura/ReporteBienesSF.cs
+++ ProyectoResidencias/Activo/BienesSinFactura/ReporteBienesSF.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using Microsoft.Reporting.WinForms;
 namespace ProyectoResidencias.Activo.BienesSinFactura
 {
     public partial class ReporteBienesSF : Form
@@ -18,8 +18,19 @@ namespace ProyectoResidencias.Activo.BienesSinFactura
 
         private void ReporteBienesSF_Load(object sender, EventArgs e)
         {
-
             this.reportViewer1.RefreshReport();
+            ReportParameter[] reporte = new ReportParameter[9];
+            reporte[0] = new ReportParameter("id", Clases.Variables.referencia);
+            reporte[1] = new ReportParameter("Etiqueta", Clases.Variables.desc);
+            reporte[2] = new ReportParameter("NoOrden", Clases.Variables.lbOrdenCompra);
+            reporte[3] = new ReportParameter("departamento", Clases.Variables.tbDepartamentoA);
+            reporte[4] = new ReportParameter("articulo", Clases.Variables.tbArticulo);
+            reporte[5] = new ReportParameter("observaciones", Clases.Variables.tbObservacion);
+            reporte[6] = new ReportParameter("nombre", Clases.Variables.tbResguardoA);
+            reporte[7] = new ReportParameter("total", "$" + Clases.Variables.desc3);
+            reporte[8] = new ReportParameter("Familia", Clases.Variables.Familia);
+            reportViewer1.LocalReport.SetParameters(reporte);
+            reportViewer1.Refresh();
         }
     }
 }
